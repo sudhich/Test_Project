88bab84ff9fb1b316004469508c115cc1b2579bf
Sudhir <sudhir1208@gmail.com>
2024-01-07 18:00:35 +0530

    first commit

---
+++ main.py
@@ -0,0 +1,29 @@
+
+from flask import Flask
+
+from wsgiref import simple_server
+
+from flask import Flask, session, request, Response, jsonify
+
+
+
+import atexit
+import uuid
+import os
+app = Flask(__name__)
+
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    print("May I know your name?")
+    return "Flask app is running and I am changing something. hi sudhir how are you?"
+
+port = int(os.getenv("PORT", 5001))
+
+if __name__ == "__main__":
+    host = '0.0.0.0'
+    #app.run()
+    httpd = simple_server.make_server(host=host, port=port, app=app)
+    #httpd = simple_server.make_server(host='127.0.0.1', port=5000, app=app)
+    # print("Serving on %s %d" % (host, port))
+    httpd.serve_forever()
